cce0fe5359dfb20ff3be59b26c57b49249561ebd
Eduard <eduard@email.com>
2026-01-21 23:34:26 +0300

    Start project

---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"flag"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+)
+
+type WsMessage struct {
+	IPAddress string `json:"address"`
+	Message   string `json:"message"`
+	Time      string `json:"time"`
+	Host      string `json:"host"`
+}
+
+var addrFlag = flag.String("a", "localhost:8443;localhost:8444", "enter addresses devided by \";\"")
+var addrList []string
+
+func main() {
+	flag.Parse()
+	addrList = strings.Split(*addrFlag, ";")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	g, gCtx := errgroup.WithContext(ctx)
+
+	dialer := websocket.Dialer{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	go func() {
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-interrupt
+		cancel()
+	}()
+
+	for _, addr := range addrList {
+		u := url.URL{Scheme: "wss", Host: addr, Path: "/ws"}
+		headers := http.Header{}
+		headers.Set("Origin", "https://"+addr)
+		log.Infof("connecting to %s", u.String())
+
+		conn, resp, err := dialer.Dial(u.String(), headers)
+		if err != nil {
+			if resp != nil {
+				log.Errorf("response: %s", resp.Status)
+			}
+			log.Fatal("dial error:", err)
+		}
+
+		g.Go(func() error {
+			defer conn.Close()
+			return sendMessages(gCtx, conn)
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		log.Info(err)
+	}
+}
+
+func sendMessages(ctx context.Context, conn *websocket.Conn) error {
+	ticker := time.NewTicker(time.Millisecond * 500)
+	defer ticker.Stop()
+
+	host, err := os.Hostname()
+	if err != nil {
+		return err
+	}
+	ip, err := hostIP()
+	if err != nil {
+		return err
+	}
+	log.Info("start sending ,messages")
+
+	for {
+		select {
+		case <-ctx.Done():
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				// log.Errorf("writing message err: %v", err)
+				return err
+			}
+			return errors.New("websocket client stopped")
+		case t := <-ticker.C:
+			msg := WsMessage{
+				IPAddress: ip,
+				Message:   t.String() + "Hello",
+				Time:      time.Now().Format("15:04"),
+				Host:      host,
+			}
+			msgByte, err := json.Marshal(msg)
+			if err != nil {
+				return err
+			}
+			err = conn.WriteMessage(websocket.TextMessage, []byte(msgByte))
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func hostIP() (string, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	return conn.LocalAddr().(*net.UDPAddr).IP.String(), nil
+}
